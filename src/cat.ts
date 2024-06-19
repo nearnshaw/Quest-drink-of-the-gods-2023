18b70aa5bc3f01917c338a466790a6e26cc0b942
nearnshaw <earnshaw.nico@gmail.com>
2024-06-19 16:14:45 -0300

    fix animations

--- src/cat.ts
+++ src/cat.ts
@@ -12,73 +12,71 @@ export let catIsOut: boolean = false
 
 export function spawnCat() {
 
-	if (catIsOut) return
-	catIsOut = true
+  if (catIsOut) return
+  catIsOut = true
 
-	const initialPosition = Vector3.create(65.8, 0.05, 74.08)
-	const initialRotation = Quaternion.fromEulerDegrees(0, -26.92, 0)
-	const endPosition = Vector3.add(initialPosition, Vector3.rotate(Vector3.Forward(), initialRotation))
+  const initialPosition = Vector3.create(65.8, 0.05, 74.08)
+  const initialRotation = Quaternion.fromEulerDegrees(0, -26.92, 0)
+  const endPosition = Vector3.add(initialPosition, Vector3.rotate(Vector3.Forward(), initialRotation))
 
-	const cat = engine.addEntity()
-	Transform.create(cat, {
-		position: initialPosition,
-		rotation: initialRotation,
-		scale: Vector3.create(0.70, 0.70, 0.70)
+  const cat = engine.addEntity()
+  Transform.create(cat, {
+    position: initialPosition,
+    rotation: initialRotation,
+    scale: Vector3.create(0.70, 0.70, 0.70)
 
-	})
+  })
 
-	GltfContainer.create(cat, {
-		src: "assets/models/NPCs/cat_orange.glb",
-		visibleMeshesCollisionMask: ColliderLayer.CL_POINTER
-	})
+  GltfContainer.create(cat, {
+    src: "assets/models/NPCs/cat_orange.glb",
+    visibleMeshesCollisionMask: ColliderLayer.CL_POINTER
+  })
 
-	MeshCollider.setBox(cat)
+  MeshCollider.setBox(cat)
 
-	pointerEventsSystem.onPointerDown({
-		entity: cat,
-		opts: {
-			button: InputAction.IA_PRIMARY,
-			hoverText: "Pick hair"
-		}
-	}, () => {
-		actionEvents.emit('action', {
-			type: 'CUSTOM',
-			parameters: { id: 'get_hair_action' },
-		})
+  pointerEventsSystem.onPointerDown({
+    entity: cat,
+    opts: {
+      button: InputAction.IA_PRIMARY,
+      hoverText: "Pick hair"
+    }
+  }, () => {
+    actionEvents.emit('action', {
+      type: 'CUSTOM',
+      parameters: { id: 'get_hair_action' },
+    })
 
-	})
+  })
 
-	utils.tweens.startTranslation(cat, initialPosition, endPosition, 2, utils.InterpolationType.LINEAR, () => {
+  utils.tweens.startTranslation(cat, initialPosition, endPosition, 2, utils.InterpolationType.LINEAR, () => {
 
-		Animator.stopAllAnimations(cat)
-		Animator.playSingleAnimation(cat, "IdleNorm")
-	})
+    Animator.stopAllAnimations(cat)
+    Animator.playSingleAnimation(cat, "IdleNorm")
+  })
 
-	Animator.create(cat, {
-		states: [
-			{
-				clip: "WalkCycle",
-				name: "WalkCycle",
-				loop: true,
-				playing: true
-			}, {
-				clip: "IdleNorm",
-				name: "IdleNorm",
-				loop: true,
-				playing: true
-			}
+  Animator.create(cat, {
+    states: [
+      {
+        clip: "WalkCycle",
+        loop: true,
+        playing: true
+      }, {
+        clip: "IdleNorm",
+        loop: true,
+        playing: true
+      }
 
-		]
-	})
+    ]
+  })
 
-	questProgress.on("step", (stepNumber: number) => {
+  questProgress.on("step", (stepNumber: number) => {
 
-		if (!catIsOut) return
-		// remove click from cat
-		if (stepNumber == StepsEnum.catGuy_step) {
-			PointerEvents.deleteFrom(cat)
-		}
-	})
+    if (!catIsOut) return
+    // remove click from cat
+    if (stepNumber == StepsEnum.catGuy_step) {
+      PointerEvents.deleteFrom(cat)
+    }
+  })
 
 
 }
